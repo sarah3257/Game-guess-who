4466fad457719b336250885f3e37bec74951c2ad
sarah <sarah134632@gmail.com>
2024-04-25 23:00:51 +0300

    my commit message

---
+++ JavaScript.js
@@ -0,0 +1,464 @@
+﻿
+https://dribbble.com/shots/5619509-Animated-Tab-Bar
+
+
+
+
+var person = function (firstName, lastName, arrD) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._arrD = arrD;
+}
+var p1 = new person('יהונתן', 'כהן', ["img/1.jpg", "בן", "גינגי", "כיפה", 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1]);
+var p2 = new person('איציק', 'ברוק', ["img/2.jpg", "בן", "חום", "כובע", 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p3 = new person('תהילה', 'ליבוביץ', ["img/3.jpg", "בת", "חום", "קישוט", 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p4 = new person('שיפי', 'וינגר', ["img/4.jpg", "בת", "גינגי", "קישוט", 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]);
+var p5 = new person('חגית', 'ארקנוי', ["img/5.jpg", "בת", "שחור", "אין", 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p6 = new person('נוח אלימלך ', 'יענקלביץ', ["img/6.jpg", "בן", "שחור", "כובע", 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p7 = new person('סבתא גודי', 'לוי', ["img/7.jpg", "בת", "לבן", "כובע", 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]);
+var p8 = new person('יעלי', 'ריפשיץ', ["img/8.jpg", "בת", "אין", "מטפחת", 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]);
+var p9 = new person('מני', 'מושקוביץ', ["img/9.jpg", "בן", "בלונדיני", "כיפה", 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p10 = new person('השדכנית נחמה', 'רוטנברג', ["img/10.jpg", "בת", "אין", "מטפחת", 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]);
+var p11 = new person('מתן', 'אברהמי', ["img/11.jpg", "בן", "בלונדיני", "אין", 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]);
+var p12 = new person('ראובן', 'בנימינוב', ["img/12.jpg", "בן", "שחור", "כיפה", 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]);
+var p13 = new person('סבתא יפה', 'יעקבזון', ["img/13.jpg", "בת", "אין", "מטפחת", 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]);
+var p14 = new person('זכריה', 'חגג', ["img/14.jpg", "בן", "שחור", "כיפה", 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1]);
+var p15 = new person('אילה', 'רפאל', ["img/15.jpg", "בת", "חום", "קישוט", 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]);
+var p16 = new person('מנחם מנדל', 'גור', ["img/16.jpg", "בן", "לבן", "כובע", 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]);
+var p17 = new person('סבתא טובה', 'לוין', ["img/17.jpg", "בת", "אין", "מטפחת", 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p18 = new person('יקי', 'אסוס', ["img/18.jpg", "בת", "חום", "כובע", 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0]);
+var p19 = new person('שמחה', 'בונים', ["img/19.jpg", "בן", "גיגני", "אין", 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
+var p20 = new person('אתי', 'אבנית', ["img/20.jpg", "בת", "בלונדיני", "אין", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p21 = new person('ישי', 'רוט', ["img/21.jpg", "בן", "חום", "כיפה", 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p22 = new person('אברהם', 'אבינו', ["img/22.jpg", "בן", "אפור", "כובע", 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p23 = new person('אלי', 'ששון', ["img/23.jpg", "בן", "חום", "כיפה", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+var p24 = new person('אביגיל', 'וינר', ["img/24.jpg", "בת", "אין", "כובע", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+
+var arrPeople =
+    [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24]
+//מין-0//1-שיער//כיסוי2//3משקפיים//זקן4//חולצה5//6נמשים//תלתלים7
+//פרחמטפחת8//9סינר//מוצץ10//שרשרת11//קישוטקיטל12//13תפילין//14פונפון
+
+var timer11;
+
+
+//מערך השאלות
+arr_questions = ["שאלות", "מין", "מהו צבע השער", "סוג הכיסוי ראש", "מרכיב משקפיים", "האם הוא מזוקן",
+    "האם יש חולצת צווארון", "נמשים", "תלתלים", "קישוט פרח במטפחת", "סינר", "מוצץ", "שרשרת", "קישוט בקיטל", "תפילין", "פונפון", "לב בחולצה"];
+arr_select1 = ["בן", "בת"]
+let cnt = 0;//מספר הניסיונות
+let randCard; //הכרטיס המוגרל
+
+//מונה הלחיצות--ברירת מחדל רמה בינונית
+let try_cnt;
+let time_cnt = 0;//נראה לי מיותר-מונה השניות
+
+//משתנים של טיימר --ברירת מחדל רמה בינונית
+let d = 0;
+let minutes=2;
+let second1=60;
+var cntT = 0;
+
+
+var life1;//הגדרת ללבות החיים
+
+//פונקציית בחירת אובייקט רנדומלי
+var saveTime=180;
+var rand = Math.floor(Math.random() * 23);
+randCard = arrPeople[rand];
+//קשה
+function hard() {
+    cntT = 0;
+    start();
+    location.replace = "HtmlPage.html";
+    try_cnt = 4;
+    minutes = 01;
+    second1 = 30;
+    saveTime = 90;
+ /*   try2();*/
+    life1 = 2;
+    life_lev(life1);//מילוי חיים
+}
+//קל
+function easy() {
+    cntT = 0;
+    start();
+    location.replace = "HtmlPage.html";
+    try_cnt = 7;
+    minutes = 04;
+    second1 = 60;
+    saveTime = 300;
+    //try2();
+    life1 = 1;
+    life_lev(life1);//מילוי חיים
+    
+}
+var set_second = 0;
+//פונקצית כניסה
+function start() {
+    document.getElementById("mainDiv").innerHTML = "";
+ 
+    document.getElementById("mainDiv").innerHTML += "<div id='clock'>טיימר</div>";
+    document.getElementById("mainDiv").innerHTML += "<div id='try'></div>";
+    document.getElementById("mainDiv").innerHTML += "<div id='people' class='img_people'></div>";
+    document.getElementById("mainDiv").innerHTML += "<div id='div_question' class='div_question'></div>";
+    document.getElementById("mainDiv").innerHTML += "<div id='div_question1' class='div_question1'></div>";
+    document.getElementById("mainDiv").innerHTML += "<img class='backImg' src='img/רקע.jpg'/>";
+    document.getElementById("mainDiv").innerHTML += "<audio id='click'><source src='audio / check.mp3' type='audio / mpeg'/></audio>";
+    document.getElementById("mainDiv").innerHTML += "<img class='tafritImg' onmouseover='openT()' id='openT' src='img/tafrit2.png'/>";
+    document.getElementById("mainDiv").innerHTML += "<div id='lifes' class='img_lifes'></div>";
+
+    try_cnt = 5;
+    if (set_second == 0) { 
+        set_second++;
+         timer11 = setInterval(second, 1000);
+    }
+    open();
+
+
+}
+
+function open()
+{
+    
+
+ //כפתורי הSELECT מין, צבע שיער וכיסוי ראש
+    document.getElementById("div_question1").innerHTML += "<select id='select1' onclick='selects(1)' class='selectQ'><option>בחר מין</option><option>בן</option><option>בת</option></select > ";
+    document.getElementById("div_question1").innerHTML += "<select id='select2' onclick='selects(2)' class='selectQ'><option>צבע שיער</option><option>חום</option><option>שחור</option><option>לבן</option><option>בלונדיני</option><option>אפור</option><option>גינגי</option><option>אין</option></select > ";
+    document.getElementById("div_question1").innerHTML += "<select id='select3' onclick='selects(3)' class='selectQ'><option>כיסוי ראש</option><option>כובע</option><option>כיפה</option><option>מטפחת</option><option>קישוט</option><option>אין</option></select > ";
+
+    //מילוי תמונות
+    filling_pictures();
+
+    
+    //שאלות כפתורים
+    for (var i = 4; i < 16; i++) {
+        document.getElementById("div_question").innerHTML += "<button onclick='buttons("+i+")' id='b" + i + "' class='question'></button>";
+        document.getElementById('b' + i).innerHTML += "<p>" + arr_questions[i] + " </p>";
+        
+    }
+   /* try1();*/
+    life1 = 0;
+    built_life();//מילוי חיים
+    life_lev(life1);//שליחת חיים ע"פ רמה
+}
+
+//מילוי תמונות
+function filling_pictures(){
+
+    //מילוי תמונות של האנשים
+    for (var i = 1; i < 25; i++) {
+        document.getElementById("people").innerHTML += "<div  id='" + i + "'  onclick='Guess(" + i + ")' class='people'></div>";
+        document.getElementById(i).innerHTML += " <img src=img/" + i + ".jpg>"
+
+    }
+    //אם נרצה את הSHOWNAME
+    //for (var i = 1; i < 25; i++) {
+    //    document.getElementById("people").innerHTML += "<div  id='" + i + "'  onmouseover='showName(" + i + ")' onclick='Guess(" + i + ")' class='people'></div>";
+    //    document.getElementById(i).innerHTML += " <img src=img/" + i + ".jpg>"
+
+    //}
+}
+
+
+
+
+
+//מגרילה
+function Guess(iImg) {
+    playAudio("Guessing");
+    if (window.confirm("האם את מתכוון לנחש את: " + arrPeople[iImg - 1]._firstName + " " + arrPeople[iImg - 1]._lastName)) {
+        var choose = arrPeople[iImg - 1]._firstName + " " + arrPeople[iImg - 1]._lastName;
+        if (choose != randCard._firstName + " " + randCard._lastName)
+            fail()
+        else
+            winner();
+
+    }
+}
+      
+ 
+
+//פונקציה שמציגה את השם של הדמות
+//function showName(id_name) {
+//    document.getElementById(id_name).innerHTML = "<p class='nameImg'>" + arrPeople[id_name - 1]._firstName + " " + arrPeople[id_name - 1]._lastName +"</p>";
+//}
+
+
+
+//פונקצית כפתורי השאלות השאלות
+function buttons(idd) {
+    
+    if (try_cnt != 0) {
+        document.getElementById('life' + try_cnt).style.display = 'none';
+
+        try_cnt--;
+       /* document.getElementById("try_div").innerHTML = 'מספר הניסיונות שנותרו:  ' + try_cnt;*/
+        playAudio("click");
+        cnt++;
+        k = randCard._arrD[idd];
+        for (var i = 0; i < 24; i++) {
+            p = arrPeople[i];
+            if (p._arrD[idd] != k) {
+                document.getElementById(i + 1).innerHTML = " <img src=img/" + 0 + ".jpg>"
+            }
+
+        }
+    }
+    else {
+        document.getElementById('life1').style.display = 'none';
+
+        playAudio("worng");
+        alert("נגמר לך חיי הניסיונות ");
+    }
+  }
+
+
+//פונקציה של כפתורי הselect שאלות
+function selects(s) {
+
+    if (try_cnt != 0) {
+
+        var dsel = document.getElementById("select" + s).value;
+        if (dsel != 'כיסוי ראש' && dsel != 'בחר מין' && dsel != 'צבע שיער') {
+            document.getElementById('life' + try_cnt).style.display = 'none';
+            try_cnt--;
+           /* document.getElementById("try_div").innerHTML = 'מספר הניסיונות שנותרו:  ' + try_cnt;*/
+            playAudio("click");
+            cnt++;
+            k = randCard._arrD[s];
+            if (document.getElementById("select" + s).value == k) {
+                for (var i = 0; i < 24; i++) {
+                    p = arrPeople[i];
+                    if (p._arrD[s] != k) {
+                        document.getElementById(i + 1).innerHTML = " <img src=img/" + 0 + ".jpg>"
+                    }
+
+                }
+            }
+            else {
+                for (var i = 0; i < 24; i++) {
+                    p = arrPeople[i];
+                    if (p._arrD[s] == document.getElementById("select" + s).value) {
+                        document.getElementById(i + 1).innerHTML = " <img src=img/" + 0 + ".jpg>"
+                    }
+
+                }
+
+            }
+            document.getElementById("select" + s).value = document.getElementById("select" + s).options[0].value;
+        }
+      
+    }
+    else {        document.getElementById('life1').style.display = 'none';
+
+        playAudio("worng");
+        alert("נגמר לך חיי הניסיונות ");
+    }
+
+}
+
+//פןנקציה שמפעיחה צליל ע"פ הID
+function playAudio(audioId) {
+    let audio = document.getElementById(audioId);
+    audio.play();
+}
+
+
+
+//פונקצית הטיימר
+function second() {
+    second1--;
+    if (second1 == 0 && minutes == 0) {
+        //להחליף שיעבור לחלונית כישלון
+        location.replace("failPage2.html");
+    }
+    if (second1 == 0) {
+        second1 = 60;
+        minutes--;
+
+    }
+    if (minutes == 0 && second1==10) {
+        alert("זמנך תם 10 שניות לפיצוץ התחל לברוח")
+
+    }
+
+    document.getElementById("clock").innerHTML = minutes + ":" + second1;
+
+
+}
+
+
+//דף הניצחון
+function winner(){
+    saveTime = saveTime - minutes * 60 + second1;
+    saveMinuts = (parseInt)(saveTime / 60);
+    saveSecond = (parseInt)(saveTime % 60);
+   
+    document.getElementById("mainDiv").innerHTML += "<div class='winnerPage1' id='winnerPage'> </div>";
+    document.getElementById("winnerPage").innerHTML += "<div class='winnerP' id='winnerP'><p>שלום ל" + localStorage.getItem("name") + " </p><p> " + minutes + " : " + second1+ " ניצחת למרות שנותרו לך עוד</p> </div>";
+
+    document.getElementById("winnerPage").innerHTML += "<img class='winImg1' src='img/winner.jpg'/>";
+    //לא מאפשר דף
+    document.getElementById("mainDiv").style.pointerEvents = 'none';
+    clearInterval(timer11);
+}
+//דף כישלון
+function fail() {
+
+    saveTime = saveTime - minutes * 60 + second1;
+    saveMinuts = (parseInt)(saveTime / 60);
+    saveSecond = (parseInt)(saveTime % 60);
+    console.log(saveMinuts + " " + saveSecond)
+    document.getElementById("mainDiv").innerHTML += "<div class='winnerPage1' id='winnerPage'> </div>";
+    document.getElementById("winnerPage").innerHTML += "<img class='winImg1' src='img/failure.jpg'/> ";
+    document.getElementById("winnerPage").innerHTML += "<div class='winnerP' id='winnerP'><p> שלום ל" + localStorage.getItem("name") + "</p><p> מצטערים לצפות בכשלונך</p> </div>";
+    //לא מאפשר דך
+    document.getElementById("mainDiv").style.pointerEvents = 'none';
+    clearInterval(timer11);
+
+}
+
+
+
+////חיים של השחקן
+function built_life() {
+    for (var i = 1; i < 8; i++) {
+        document.getElementById("lifes").innerHTML += "<div  id='life" + i + "'  class='live'></div>";
+        document.getElementById('life' + i).innerHTML += " <img class='lifes1' id='live" + i + "' src=img/live.png>"
+
+
+    }
+   
+   
+    
+}
+
+//חיים עם שינוי רמות
+function life_lev(life1) {
+    if (life1 == 0) {//רמה בינונית
+        for (var i = 6; i < 8; i++) {
+            document.getElementById('life' + i).style.display = 'none';
+
+        }
+        document.getElementById('life5').style.display = 'inline-block';
+    }
+    else if (life1 == 1) {
+
+        for (var i = 1; i < 8; i++) {
+            document.getElementById('life' + i).style.display = 'inline-block';
+        }
+    }
+    else if (life1 == 2) {//רמה קשה
+        for (var i = 1; i < 5; i++) {
+            document.getElementById('life' + i).style.display = 'inline-block';
+        }
+        for (var i = 5; i < 8; i++) {
+            document.getElementById('life' + i).style.display = 'none';
+
+        }
+    }
+}
+//תפריט 
+function openT() {
+    if (cntT == 0) { 
+        document.getElementById("mainDiv").innerHTML += "<div id='tafrit' onclick='closeT()'> <a href='#'id='circle' class='muenButton'><i id='x' class='fa fa-times-circle'></i></a><br/>  <br/>  <a href='#' onclick='open_instrecthion()' class='muenButton'><i class='fa fa-gamepad'></i></a><br/> <br/>       <a href='#' id='lev1' onclick='easy()' class='muenButton'> <i class='fa fa-hand-o-up'></i> </a>  <br/> <br/>       <a href='#' onclick='hard()'  class='muenButton'><i  class='fa fa-hand-peace-o'></i></a> <br/> <br/> <a href='#' onclick='musicBackg()'  class='muenButton'><i class='fa fa-music'></i> </a><br/> <br/> <br/>    <a href='home.html' class='muenButton'><i class='fa fa-address-card-o'></i></a>  </div>";
+        cntT++;
+    }
+    else
+        document.getElementById("tafrit").style.display ='inline-block';
+
+}
+
+function closeT()
+{
+    document.getElementById("tafrit").style.display='none';
+}
+function musicBackg() {
+    playAudio("musicB");
+}
+
+//פתיחת הוראות
+function open_instrecthion() {
+    document.getElementById("instrecthion1").style.display = 'inline-block';
+
+}
+//סגירת הוראות 
+function close_instrecthion() {
+    document.getElementById("instrecthion1").style.display = 'none';
+ 
+}
+
+function savePlay() {
+    //מערך
+    playAudio("click");
+    var arr = [];
+    for (var i = 1; i < 25; i++) {
+        var st = document.getElementById(i).children[0].src;
+        st = st.substring(st.lastIndexOf("img"));
+        arr[i] = st;
+    }
+    var heart = [];
+    for (var i = 1; i < 8; i++) {
+        /*if (document.getElementById('life' + i).style.display == "none")*/
+        heart[i] = document.getElementById('life' + i).style.display == "none" ? "none" :"inline-block";
+
+        
+
+
+     
+    }
+
+
+    //divs = document.querySelector("body>div>div:nth-of-type(3)").children;
+    //for (var i = 0; i < length; i++) {
+    //    arr.push(divs[i].children[0].value);
+    //}
+    //לשמור שניות
+    //לשמור כל הפרטים
+    //שמיר:
+    localStorage.setItem("arr", arr);
+    localStorage.setItem("rand", rand);
+    localStorage.setItem("minutes", minutes);
+    localStorage.setItem("second1", second1);
+    localStorage.setItem("try_cnt", try_cnt);
+    localStorage.setItem("heart", heart);
+
+    //משתנים
+
+}
+function saveName() {
+    localStorage.setItem("name", document.getElementById("txt-input").value);
+}
+
+function backtoplay() {
+    playAudio("click");
+    if (localStorage.getItem("arr")) {
+        ///*      arr = JSON.parse(localStorage.getItem("arr"));
+        arr = localStorage.getItem("arr");
+        arry = arr.split(",");
+        heart = localStorage.getItem("heart");
+        hearty = heart.split(",");
+        //כל המדתנים
+        //למלא לוח
+
+        for (var i = 1; i < 25; i++) {
+
+            document.getElementById("people").innerHTML += "<div  id='" + i + "'  onclick='Guess(" + i + ")' class='people'></div>";
+            document.getElementById(i).innerHTML = " <img src=" + arry[i] + ">"
+
+        }
+        rand = localStorage.getItem("rand");
+        minutes = localStorage.getItem("minutes");
+        second1 = localStorage.getItem("second1");
+        randCard = arrPeople[rand];
+        try_cnt = localStorage.getItem("try_cnt");
+
+        for (var i = 1; i < 8; i++) {
+            
+            hearty[i] == "none" ? document.getElementById('life' + i).style.display = "none" : document.getElementById('life' + i).style.display= "inline-block";
+
+        }
+    }
+
+}
